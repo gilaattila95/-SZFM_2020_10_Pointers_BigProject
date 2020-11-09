78a481bdb7ebbee036e5718b2c74392adc9c8974
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2020-11-09 10:31:01 +0100

    Hivatkozások javítása a kilépés gombnál

--- src/admin/add_admin.php
+++ src/admin/add_admin.php
@@ -36,7 +36,7 @@
 			</button>
 		 </a>
 
-		<a href="admin.php?event=kilepes" name="event" id="event" value="kilepes">
+		<a href="../index.php?event=kilepes" name="event" id="event" value="kilepes">
             <button type="button" class="button2">Admin kilépés</button>
 		</a>
 
