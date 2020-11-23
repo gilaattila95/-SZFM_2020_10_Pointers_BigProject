99add6f356dc62ef8e19761f6e85671d2256d099
Gila Attila <gila.attila95@gmail.com>
2020-11-23 09:09:45 +0100

    felcserélt fájlok javítása

--- src/action.php
+++ src/action.php
@@ -1,103 +1,37 @@
+<?php
+ session_start();
+ 
+ // Adatbázis csatlakozás
+$dbc = mysqli_connect("localhost", "root", "", "bigproject");
+$sql = "set names utf8";
+mysqli_query($dbc, $sql);
 
-<?php 
-	session_start(); // starting the session, necessary for using session variables
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 
-	// DBMS connection code -> hostname, username, password, database name
-	$db = mysqli_connect('localhost', 'root', '', 'bigproject');
-	$sql = "set names utf8";
-	mysqli_query($db, $sql);
 
-	// declaring and hoisting the variables
-	$username = "";
-	$email    = "";
-	$errors = array(); 
-	$_SESSION['success'] = "";
+if (pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME) == "index") {
+   
+    if ($result = mysqli_query($dbc, "SELECT * FROM cikkek WHERE kategoria ='erdekes' and aktiv = 1")) {
+        $row_cnt = mysqli_num_rows($result);
+        mysqli_free_result($result);
+    }
 
-	// registration code
-	if (isset($_POST['reg_user'])) {
+    $sql = "select * from cikkek where kategoria = 'erdekes' and aktiv = 1 order by RAND() LIMIT $row_cnt";
+    $erdekes = mysqli_query($dbc, $sql);
+}
 
-		// receiving the values entered and storing in the variables
-		//data sanitization is done to prevent SQL injections
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$email = mysqli_real_escape_string($db, $_POST['email']);
-		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
-
-		// ensuring that the user has not left any input field blank
-		// error messages will be displayed for every blank input
-		if (empty($username)) { array_push($errors, "Username is required"); }
-		if (empty($email)) { array_push($errors, "Email is required"); }
-		if (empty($password_1)) { array_push($errors, "Password is required"); }
-	
-		//checking if the passwords match
-		if ($password_1 != $password_2) {
-			array_push($errors, "The two passwords do not match");
-		}
-		
-		$sql_u = "SELECT * FROM ceg_userek WHERE uname='$username'";
-		$sql_e = "SELECT * FROM ceg_userek WHERE email='$email'";
-		$res_u = mysqli_query($dbc, $sql_u);
-		$res_e = mysqli_query($dbc, $sql_e);
+if (pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME) == "vicces") {
   
-		if (mysqli_num_rows($res_u) > 0) {
-			array_push($errors, "Username already taken!"); 	
-		}
-		
-		if(mysqli_num_rows($res_e) > 0){
-			array_push($errors, "Email already taken!"); 	
-		}	
+    if ($result = mysqli_query($dbc, "SELECT * FROM cikkek WHERE kategoria ='vicces' and aktiv = 1")) {
+        $row_cnt = mysqli_num_rows($result);
+        mysqli_free_result($result);
+    }
 
-		// if the form is error free, then register the user
-		if (count($errors) == 0) {
-			$password = md5($password_1);//password encryption to increase data security
-			$query = "INSERT INTO ceg_userek (`email`, `jelszo`, `uname`, `aktiv`) 
-					  VALUES('$email', '$password','$username', '1' )"; //inserting data into table
-			mysqli_query($db, $query);
-
-			//storing username of the logged in user, in the session variable
-			$_SESSION['username'] = $username;
-			header('location: ../src/user/user_article.php'); 
-			//page on which the user will be redirected after logging in
-		}
-
-	}
-
-	// user login
-	if (isset($_POST['login_user'])) {
-		//data sanitization to prevent SQL injection
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$password = mysqli_real_escape_string($db, $_POST['password']);
-
-		//error message if the input field is left blank
-		if (empty($username)) {
-			array_push($errors, "Username is required");
-		}
-		if (empty($password)) {
-			array_push($errors, "Password is required");
-		}
-
-		//checking for the errors
-		if (count($errors) == 0) {
-			$password = md5($password); //password matching
-			$query = "SELECT * FROM ceg_userek WHERE uname='$username' AND jelszo='$password'";
-			$results = mysqli_query($db, $query);
-
-			// $results = 1 means that one user with the entered username exists
-			if (mysqli_num_rows($results) == 1) {
-				$_SESSION['username'] = $username; //storing username in session variable
-				header('location:../src/user/user_article.php'); //page on which the user is sent to after logging in
-			}else {
-				array_push($errors, "Username or password incorrect"); 
-				//if the username and password doesn't match
-			}
-		}
-	}
-
-    //HA HOSSZÚ A SZÖVEG, LEGYEN RÖVID ÉS '...'
-    function truncate($str, $chars, $end = '...') {
-        if (strlen($str) <= $chars) return $str;
-        $new = substr($str, 0, $chars + 1);
-        return substr($new, 0, strrpos($new, ' ')) . $end;
-	}
+    $sql = "select * from cikkek where kategoria = 'vicces' and aktiv = 1 order by RAND() LIMIT $row_cnt";
+    $erdekes = mysqli_query($dbc, $sql);
+}
 
 ?>
--- src/user/action.php
+++ src/user/action.php
@@ -1,75 +1,103 @@
-<?php
- session_start();
- 
- // Adatbázis csatlakozás
-$dbc = mysqli_connect("localhost", "root", "", "bigproject");
-$sql = "set names utf8";
-mysqli_query($dbc, $sql);
 
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
+<?php 
+	session_start(); // starting the session, necessary for using session variables
 
-$uzenet = "";
+	// DBMS connection code -> hostname, username, password, database name
+	$db = mysqli_connect('localhost', 'root', '', 'bigproject');
+	$sql = "set names utf8";
+	mysqli_query($db, $sql);
 
-// Eseménykezelés
-$pEvent = htmlspecialchars(filter_input(INPUT_POST, "event"));
-$gEvent = htmlspecialchars(filter_input(INPUT_GET, "event"));
-$uname = htmlspecialchars(filter_input(INPUT_POST, "uname"));
-$psw = htmlspecialchars(filter_input(INPUT_POST, "psw"));
-$ctext = htmlspecialchars(filter_input(INPUT_POST, "ctext"));
-$kategoria = filter_input(INPUT_POST, "kategoria");
+	// declaring and hosting the variables
+	$username = "";
+	$email    = "";
+	$errors = array(); 
+	$_SESSION['success'] = "";
 
+	// registration code
+	if (isset($_POST['reg_user'])) {
 
-if ($gEvent == "kilepes") {
-    unset($_SESSION["cuid"]);
-    unset($_SESSION["uname"]);
-    header("location:index.php");
-}
+		// receiving the values entered and storing in the variables
+		//data sanitization is done to prevent SQL injections
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$email = mysqli_real_escape_string($db, $_POST['email']);
+		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-if ($pEvent == "bejelentkezés") {
-    $sql = "select * from ceg_userek where uname = '$uname' and aktiv = 1";
-    
-    if($result = mysqli_query($dbc, $sql)) {
-        
-        $row = mysqli_fetch_row($result);
-        if(password_verify($psw, $row[2])) {// Beléphet
-            $_SESSION["cuid"] = $row[0];
-            $_SESSION["uname"] = $row[3];
-            if(isset($_SESSION["cuid"])) {
-                header("location:../src/user/user_article.php");
-                exit;
-            }
-        } else { // Nem léphet be
-            $uzenet = "Sikertelen belépés!";
-        }  
-    }
+		// ensuring that the user has not left any input field blank
+		// error messages will be displayed for every blank input
+		if (empty($username)) { array_push($errors, "Username is required"); }
+		if (empty($email)) { array_push($errors, "Email is required"); }
+		if (empty($password_1)) { array_push($errors, "Password is required"); }
+	
+		//checking if the passwords match
+		if ($password_1 != $password_2) {
+			array_push($errors, "The two passwords do not match");
+		}
+		
+		$sql_u = "SELECT * FROM ceg_userek WHERE uname='$username'";
+		$sql_e = "SELECT * FROM ceg_userek WHERE email='$email'";
+		$res_u = mysqli_query($dbc, $sql_u);
+		$res_e = mysqli_query($dbc, $sql_e);
+  
+		if (mysqli_num_rows($res_u) > 0) {
+			array_push($errors, "Username already taken!"); 	
+		}
+		
+		if(mysqli_num_rows($res_e) > 0){
+			array_push($errors, "Email already taken!"); 	
+		}	
 
-} else if ($gEvent == "kilepes") {
-        unset($_SESSION["cuid"]);
-        unset($_SESSION["uname"]);
-        header("location:index.php");
-    }
+		// if the form is error free, then register the user
+		if (count($errors) == 0) {
+			$password = md5($password_1);//password encryption to increase data security
+			$query = "INSERT INTO ceg_userek (`email`, `jelszo`, `uname`, `aktiv`) 
+					  VALUES('$email', '$password','$username', '1' )"; //inserting data into table
+			mysqli_query($db, $query);
 
+			//storing username of the logged in user, in the session variable
+			$_SESSION['username'] = $username;
+			header('location: ../src/user/user_article.php'); 
+			//page on which the user will be redirected after logging in
+		}
 
-//User saját cikkeinek listázása
-if (pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME) == "user_article") {
-    if (isset($_SESSION["cuid"])) {
-        $sql="select * from cikkek where aktiv=1 and usent=(select cuid from ceg_userek where uname = '$uname' and aktiv = 1)"; 
-        $cikkek = mysqli_query($dbc, $sql);
-    } 
-}
+	}
 
-$uzenet = "";
+	// user login
+	if (isset($_POST['login_user'])) {
+		//data sanitization to prevent SQL injection
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$password = mysqli_real_escape_string($db, $_POST['password']);
 
+		//error message if the input field is left blank
+		if (empty($username)) {
+			array_push($errors, "Username is required");
+		}
+		if (empty($password)) {
+			array_push($errors, "Password is required");
+		}
 
-//HA HOSSZÚ A SZÖVEG, LEGYEN RÖVID ÉS '...'
-function truncate($str, $chars, $end = '...') {
-    if (strlen($str) <= $chars) return $str;
-    $new = substr($str, 0, $chars + 1);
-    return substr($new, 0, strrpos($new, ' ')) . $end;
-}
+		//checking for the errors
+		if (count($errors) == 0) {
+			$password = md5($password); //password matching
+			$query = "SELECT * FROM ceg_userek WHERE uname='$username' AND jelszo='$password'";
+			$results = mysqli_query($db, $query);
+
+			// $results = 1 means that one user with the entered username exists
+			if (mysqli_num_rows($results) == 1) {
+				$_SESSION['username'] = $username; //storing username in session variable
+				header('location:../src/user/user_article.php'); //page on which the user is sent to after logging in
+			}else {
+				array_push($errors, "Username or password incorrect"); 
+				//if the username and password doesn't match
+			}
+		}
+	}
+
+    //HA HOSSZÚ A SZÖVEG, LEGYEN RÖVID ÉS '...'
+    function truncate($str, $chars, $end = '...') {
+        if (strlen($str) <= $chars) return $str;
+        $new = substr($str, 0, $chars + 1);
+        return substr($new, 0, strrpos($new, ' ')) . $end;
+	}
 
 ?>
-
